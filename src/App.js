642f224ccf6f1000073f091ec0eefbd8578724ee
Vinay <vk932438@gmail.com>
2024-04-01 18:37:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(2);
+  const addValue = () => {
+    setCount(count + 1);
+  };
+  const removeValue = () => {
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Welcome to our Counter app!</h1>
+        <h1>Counter Value : {count}</h1>
+        <button onClick={addValue}>Add Value</button>
+        <br />
+        <button onClick={removeValue}>Remove Value</button>
+      </div>
+    </>
   );
 }
 
